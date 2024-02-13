751cd4971017027cf5c033dbff8d2d99593045b6
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2024-02-12 23:02:17 -0600

    Add implied odds

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ function App() {
           <p>Probability of winning: {(probability * 100).toFixed(2)}%</p>
         </div>
         <div>
-          <p>Payout on win: {payout.toFixed(2)}x the bet (${(payout * userBet).toFixed(2)}) (including your bet)</p>
+          <p>Payout on win: {payout.toFixed(2)}x the bet (${(payout * userBet).toFixed(2)}) (including your bet) ({(100.0 / payout).toFixed(2)}% implied odds)</p>
         </div>
         <div>
           <label>Your bet: ${userBet.toFixed(2)} ({((userBet / bankroll) * 100).toFixed(2)}% of bankroll)</label>
